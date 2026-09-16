5ce158599b85f046f69e1e77b9be0f711a6732b0
Lasry BESKIWIN <lasry.beskiwin-yestin@epitech.eu>
2026-09-16 15:15:20 +0200

    chore: bootstrap Spring Boot project

---
+++ src/main/java/fr/lasrybeskiwin/ledgercore/LedgerCoreApplication.java
@@ -0,0 +1,13 @@
+package fr.lasrybeskiwin.ledgercore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgerCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedgerCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/lasrybeskiwin/ledgercore/LedgerCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.lasrybeskiwin.ledgercore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
